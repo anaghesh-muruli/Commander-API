eb7bc5f811d2e03c1d65a7eaa151b3668892b61a
anagheshm <anagheshm@gmail.com>
2020-11-16 23:47:39 +0530

    Init commit

---
+++ Data/CommanderRepoImpl.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class CommanderRepoImpl : ICommanderRepo
+    {
+        private readonly CommanderContext _context;
+
+        public CommanderRepoImpl(CommanderContext context)
+        {
+            _context = context;
+
+        }
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command getCommandByID(int id)
+        {
+            return _context.Commands.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
